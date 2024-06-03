9a24a372b68b5fb2970eca0af194e9b32f8a09b5
luotuoqingshan <2019hyf@gmail.com>
2024-06-03 14:57:06 -0400

    fix one name mismatch problem

--- SCAMS/gen_test.jl
+++ SCAMS/gen_test.jl
@@ -2,7 +2,7 @@ graphs = ["G$i" for i = 1:9] # graphs you want to test on
 tol = 1e-2
 seed = 0
 
-open(homedir()*"/SCAMSv2-new/SCAMS/test_MaxCut.txt", "w") do io
+open(homedir()*"/SCAMSv2/SCAMS/test_MaxCut.txt", "w") do io
     for graph in graphs
         println(io, "ulimit -d $((16 * 1024 * 1024));"*
         "cd ~/SCAMSv2/SCAMS;"*
